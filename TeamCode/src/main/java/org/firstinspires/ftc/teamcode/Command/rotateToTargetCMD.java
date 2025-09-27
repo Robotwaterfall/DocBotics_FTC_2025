59ba9fb0f7921282827f04872d1957985ec5db8c
Robotwaterfall <jaysonwelshman@gmail.com>
2025-09-27 10:24:58 -0400

    changed the clipped power to 30% when using limelight for more accurate results

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Command/rotateToTargetCMD.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Command/rotateToTargetCMD.java
@@ -22,17 +22,18 @@ public class rotateToTargetCMD extends CommandBase {
         if(llSub.hasTarget()) {
             double error = llSub.getTx(); //horizontal offset in degrees
             double rotPower = error * kPRotation;
-            //clipped power to [-0.4, 0.4] for safety
-            rotPower = Math.max(Math.min(rotPower, 0.4), -0.4);
+            //clipped power to [-0.3, 0.3] for safety
+            rotPower = Math.max(Math.min(rotPower, 0.3), -0.3);
 
             drive.drive(0, 0, rotPower); //rot only
         } else {
-            drive.drive(0,0,0); //stop if not target is found
+            drive.drive(0,0,0); //stop if no target is found
         }
     }
 
     @Override
     public boolean isFinished(){
+        //if limelight returns with target and the Tx is less then the deadband the command will finish
         return llSub.hasTarget() && Math.abs(llSub.getTx()) < deadband;
     }
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -5,14 +5,14 @@ public class Constants {
     // =======Limelight Settings=======
     public static final double camera_Height        = 0.52;
     public static final double target_Height        = 0.82;
-    public static final double camera_Angle         = 75.0;
+    public static final double camera_Angle         = 92.0;
 
     // =======PID Gains=======
     public static final double kPRotation           = 0.05;
 
     // =======Tolerance and Deadband=======
     public static final double tolerance            = 0.05; //offset of the rotation and forwarding
-    public static final double deadband             = 1.0; //degrees in which we stop strafing
+    public static final double deadband             = 1.0; //degrees in which we stop rotating
 
     // =======Target Setpoints=======
     public static final double targetDistanceMeters = 2;
