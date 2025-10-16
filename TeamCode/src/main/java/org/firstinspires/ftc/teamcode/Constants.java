7c5ee292ff90425e74f8538ef3252cf92a66d999
Caden Corpuz <corpuzcaden@gmail.com>
2025-10-16 14:53:01 -0400

    lowered angular scaler

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Constants.java
@@ -6,7 +6,7 @@ import com.acmerobotics.dashboard.config.Config;
 public class Constants {
 
     public static double  soos_Linear_scaler = 1.1975;
-    public static double  soos_Angular_scaler = 0.99722;
+    public static double  soos_Angular_scaler = 0.98622;
     private Constants() { }
 
 }
